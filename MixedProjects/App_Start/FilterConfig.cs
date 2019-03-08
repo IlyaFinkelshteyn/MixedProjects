d49ec17358df608b6d3514f4606d09b380a1a78d
IlyaFinkelshteyn <ilyaf@appveyor.com>
2019-03-07 16:13:10 -0800

    initial commit

---
+++ MixedProjects/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MixedProjects
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
